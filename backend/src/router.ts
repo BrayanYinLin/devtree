ab192c9a4d7fcdf6544f90f774cdb9c31616b0d8
Vincx0 <vincent04.aa@gmail.com>
2025-11-21 04:03:11 -0500

    parte completa de la hoja 1

--- backend/src/router.ts
+++ backend/src/router.ts
@@ -1,8 +1,9 @@
 import {Router} from 'express'
 import {body} from 'express-validator' //body permite validar el req.body
 import User from './models/User'
-import { createAccount, login } from './handlers'
+import { createAccount, getUser, login, updateProfile } from './handlers'
 import { handleInputErrors } from './middleware/validation'
+import { authenticate } from './middleware/auth'
 
 //Permite configurar un objeto con todas las rutas que después podemos agregar a la app principal server.ts
 
@@ -23,6 +24,16 @@ router.post('/auth/login',
     body('email').isEmail().withMessage('Email no válido'),
     body('password').notEmpty().withMessage('El password es muy corto, mínimo 8 caracteres'),
 
-    login)
+    login
+)
+
+router.get('/user', authenticate ,getUser)
+
+router.patch('/user',
+    body('handle').notEmpty().withMessage('El handle no puede ir vacio'),
+    body('description').notEmpty().withMessage('La descripcion no puede ir vacia'),
+    handleInputErrors,
+    
+    authenticate, updateProfile)
 
 export default router
